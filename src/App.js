b0118856e3d16ac1f849a242542a9371e636dd72
Anastasia Orlova <anastasia2804@gmail.com>
2022-09-28 20:42:26 -0400

    Iterations 1-4 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import contacts from './contacts.json';
+
+const remainingContacts = contacts.slice(5)
 
 function App() {
+
+  const [contactsArr, setContacts] = useState(contacts.slice(0,5))
+
+  function addRandomContact () {
+    let randomContactIndex = Math.floor(Math.random()*remainingContacts.length)
+    let randomContact = remainingContacts.splice(randomContactIndex, 1)[0]
+    
+    const copyContactsArr = [...contactsArr, randomContact]
+
+    setContacts(copyContactsArr)
+
+    console.log(remainingContacts)
+  }
+
+  function sortPopularity() {
+    let sortedArr = contacts.sort(function(a,b){
+      return b.popularity-a.popularity
+    })
+
+    const copySortedArr = [...sortedArr]
+    setContacts(copySortedArr)
+  }
+
+  function sortName () {
+    let sortedArr = contacts.sort(function(a, b) {
+      let x = a.name.toLowerCase();
+      let y = b.name.toLowerCase();
+      if (x < y) {return -1;}
+      if (x > y) {return 1;}
+      return 0;
+    })
+
+    const copySortedArr = [...sortedArr]
+    setContacts(copySortedArr)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Iron Contacts</h1>
+    <div className='btn-row'>
+    <button onClick={addRandomContact}>Add Random Contact</button>
+    <button onClick={sortPopularity}>Sort by popularity</button>
+    <button onClick={sortName}>Sort by name</button>
+    </div>
+  
+      <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+        </thead>
+
+        <tbody>
+       {contactsArr.map(element => {
+        return (
+          <tr>
+            <td><img src={element.pictureUrl} height="100px" alt="celebrity"/></td>
+            <td>{element.name}</td>
+            <td>{element.popularity.toFixed(2)}</td>
+            <td>
+            {element.wonOscar ? <img src="/images/oscar.png" height="30px" alt="award"/>: ""}
+            </td>
+            <td>
+            {element.wonEmmy && <img src="/images/oscar.png" height="30px" alt="award"/>}
+            {!element.wonEmmy && ""}
+            </td>
+          </tr>
+        )
+       })}
+        </tbody>
+</table>
+
+       
     </div>
   );
 }
